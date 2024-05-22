30d9b658b8bc4cebb497bb9ec53191c448340f87
gor9an <gor9an@mail.ru>
2024-05-23 00:51:23 +0300

    add Tracker models

---
+++ Tracker/Trackers/Models/Tracker.swift
@@ -0,0 +1,8 @@
+//
+//  Tracker.swift
+//  Tracker
+//
+//  Created by Andrey Gordienko on 23.05.2024.
+//
+
+import Foundation
---
+++ Tracker/Trackers/Models/TrackerRecord.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerRecord.swift
+//  Tracker
+//
+//  Created by Andrey Gordienko on 23.05.2024.
+//
+
+import Foundation
